2659fadb29f62891b9ebeeb81139fe26ee392888
Stephanie Fay <stephaniemfay@gmail.com>
2021-04-13 22:31:09 -0300

    Firebase configuration & mapping of collections and icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AngularFireModule} from "@angular/fire";
+import {AngularFireDatabaseModule} from "@angular/fire/database";
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/collections.ts
@@ -0,0 +1,155 @@
+export const Collections = {
+
+  // Base Collection
+  BASE: {value: "base1", label: "Base"} ,
+  JUNGLE: {value: "base2", label: "Jungle"},
+  BASE_PROMOS: {value: "basep", label: "Wizards Black Star Promos"},
+  FOSSIL: {value: "base3", label: "Fossil"},
+  BASE_2: {value: "base4", label: "Base Set 2"},
+  TEAM_ROCKET: {value: "base5", label: "Team Rocket"},
+
+  // Gym Collection
+  GYM_HEROES: {value: "gym1", label: "Gym Heroes"},
+  GYM_CHALLENGE: {value: "gym2", label: "Gym Challenge"},
+
+  // Neo Collection
+  NEO_GENESIS: {value: "neo1", label: "Neo Genesis"},
+  NEO_DISCOVERY: {value: "neo2", label: "Neo Discovery"},
+  NEO_REVELATION: {value: "neo3", label: "Neo Revelation"},
+  NEO_DESTINY: {value: "neo4", label: "Neo Destiny"},
+
+  // E-Card Collection
+  EXPEDITION_BASE: {value: "ecard1", label: "Expedition Base Set"},
+  AQUAPOLIS: {value: "ecard2", label: "Aquapolis"},
+  SKYRIDGE: {value: "ecard3", label: "Skyridge"},
+
+  // NP Collection
+  NINTENDO_PROMOS: {value: "np", label: "Nintendo Black Star Promos"},
+
+  // EX Collection
+  RUBY_SAPPHIRE: {value: "ex1", label: "Ruby & Sapphire"},
+  SANDSTORM: {value: "ex2", label: "Sandstorm"},
+  DRAGON: {value: "ex3", label: "Dragon"},
+  MAGMA_AQUA: {value: "ex4", label: "Team Magma vs Team Aqua"},
+  HIDDEN_LEGENDS: {value: "ex5", label: "Hidden Legends"},
+  FIRERED_LEAFGREEN: {value: "ex6", label: "FireRed & LeafGreen"},
+  TEAMROCKET_RETURNS: {value: "ex7", label: "Team Rocket Returns"},
+  DEOXYS: {value: "ex8", label: "Deoxys"},
+  EMERALD: {value: "ex9", label: "Emerald"},
+  UNSEEN_FORCES: {value: "ex10", label: "Unseen Forces"},
+  DELTA_SPECIES: {value: "ex11", label: "Delta Species"},
+  LEGEND_MAKER: {value: "ex12", label: "Legend Maker"},
+  HOLON_PHANTOMS: {value: "ex13", label: "Holon Phantoms"},
+  CRYSTAL_GUARDIANS: {value: "ex14", label: "Crystal Guardians"},
+  DRAGON_FRONTIERS: {value: "ex15", label: "Dragon Frontiers"},
+  POWER_KEEPERS: {value: "ex16", label: "Power Keepers"},
+
+  // Diamond & Pearl Collection
+  DP_PROMOS: {value: "dpp", label: "DP Black Star Promos"},
+  DIAMOND_PEARL: {value: "dp1", label: "Diamond & Pearl"},
+  MYSTERIOUS_TREASURES: {value: "dp2", label: "Mysterious Treasures"},
+  SECRET_WONDERS: {value: "dp3", label: "Secret Wonders"},
+  GREAT_ENCOUNTERS: {value: "dp4", label: "Great Encounters"},
+  MAJESTIC_DAWN: {value: "dp5", label: "Majestic Dawn"},
+  LEGENDS_AWAKENED: {value: "dp6", label: "Legends Awakened"},
+  STORMFRONT: {value: "dp7", label: "Stormfront"},
+
+  // POP Collection
+  POP1: {value: "pop1", label: "POP Series 1"},
+  POP2: {value: "pop2", label: "POP Series 2"},
+  POP3: {value: "pop3", label: "POP Series 3"},
+  POP4: {value: "pop4", label: "POP Series 4"},
+  POP5: {value: "pop5", label: "POP Series 5"},
+  POP6: {value: "pop6", label: "POP Series 6"},
+  POP7: {value: "pop7", label: "POP Series 7"},
+  POP8: {value: "pop8", label: "POP Series 8"},
+  POP9: {value: "pop9", label: "POP Series 9"},
+
+  // Platinum Collection
+  PLATINUM: {value: "pl1", label: "Platinum"},
+  RISING_RIVALS: {value: "pl2", label: "Rising Rivals"},
+  SUPREME_VICTORS: {value: "pl3", label: "Supreme Victors"},
+  ARCEUS: {value: "pl4", label: "Arceus"},
+
+  // HeartGold & SoulSilver Collection
+  HGSS_PROMOS: {value: "hsp", label: "HGSS Black Star Promos"},
+  HEARTGOLD_SOULSILVER: {value: "hgss1", label: "HeartGold & SoulSilver"},
+  HS_UNLEASHED: {value: "hgss2", label: "HS—Unleashed"},
+  HS_UNDAUNTET: {value: "hgss3", label: "HS—Undaunted"},
+  HS_TRIUMPHANT: {value: "hgss4", label: "HS—Triumphant"},
+  CALL_LEGENDS: {value: "col1", label: "Call of Legends"},
+
+  // Black & White Collection
+  BW_PROMOS: {value: "bwp", label: "BW Black Star Promos"},
+  BLACK_WHITE: {value: "bw1", label: "Black & White"},
+  EMERGING_POWERS: {value: "bw2", label: "Emerging Powers"},
+  NOBLE_VICTORIES: {value: "bw3", label: "Noble Victories"},
+  NEXT_DESTINIES: {value: "bw4", label: "Next Destinies"},
+  DARK_EXPLORERS: {value: "bw5", label: "Dark Explorers"},
+  DRAGONS_EXALTED: {value: "bw6", label: "Dragons Exalted"},
+  DRAGON_VAULT: {value: "dv1", label: "Dragon Vault"},
+  BOUNDARIES_CROSSED: {value: "bw7", label: "Boundaries Crossed"},
+  PLASMA_STORM: {value: "bw8", label: "Plasma Storm"},
+  PLASMA_FREEZE: {value: "bw9", label: "Plasma Freeze"},
+  PLASMA_BLAST: {value: "bw10", label: "Plasma Blast"},
+  LEGENDARY_TREASURES: {value: "bw11", label: "Legendary Treasures"},
+
+  // XY Collection
+  XY_PROMOS: {value: "xyp", label: "XY Black Star Promos"},
+  KALOS_STARTER: {value: "xy0", label: "Kalos Starter Set"},
+  XY: {value: "xy1", label: "XY"},
+  FLASHFIRE: {value: "xy2", label: "Flashfire"},
+  FURIOUS_FISTS: {value: "xy3", label: "Furious Fists"},
+  PHANTOM_FORCES: {value: "xy4", label: "Phantom Forces"},
+  PRIMAL_CLASH: {value: "xy5", label: "Primal Clash"},
+  DOUBLE_CRISIS: {value: "dc1", label: "Double Crisis"},
+  ROARING_SKIES: {value: "xy6", label: "Roaring Skies"},
+  ANCIENT_ORIGINS: {value: "xy7", label: "Ancient Origins"},
+  BREAKTHROUGH: {value: "xy8", label: "BREAKthrough"},
+  BREAKPOINT: {value: "xy9", label: "BREAKpoint"},
+  GENERATIONS: {value: "g1", label: "Generations"},
+  FATES_COLLIDE: {value: "xy10", label: "Fates Collide"},
+  STEAM_SIEGE: {value: "xy11", label: "Steam Siege"},
+  EVOLUTIONS: {value: "xy12", label: "Evolutions"},
+
+  // Other Collection
+  SOUTHERN_ISLANDS: {value: "si1", label: "Southern Islands"},
+  LEGENDARY_COLLECTION: {value: "base6", label: "Legendary Collection"},
+  POKEMON_RUMBLE: {value: "ru1", label: "Pokémon Rumble"},
+  MCD_2011: {value: "mcd11", label: "McDonald's Collection 2011"},
+  MCD_2012: {value: "mcd12", label: "McDonald's Collection 2012"},
+  MCD_2016: {value: "mcd16", label: "McDonald's Collection 2016"},
+  MCD_2019: {value: "mcd19", label: "McDonald's Collection 2019"},
+
+  // Sun & Moon Collection
+  SM_PROMOS: {value: "smp", label: "SM Black Star Promos"},
+  SUN_MOON: {value: "sm1", label: "Sun & Moon"},
+  GUARDIANS_RISING: {value: "sm2", label: "Guardians Rising"},
+  BURNING_SHADOWS: {value: "sm3", label: "Burning Shadows"},
+  SHINING_LEGENDS: {value: "sm35", label: "Shining Legends"},
+  CRIMSON_INVASION: {value: "sm4", label: "Crimson Invasion"},
+  ULTRA_PRISM: {value: "sm5", label: "Ultra Prism"},
+  FORBIDDEN_LIGHT: {value: "sm6", label: "Forbidden Light"},
+  CELESTIAL_STORM: {value: "sm7", label: "Celestial Storm"},
+  DRAGON_MAJESTY: {value: "sm75", label: "Dragon Majesty"},
+  LOST_THUNDER: {value: "sm8", label: "Lost Thunder"},
+  TEAM_UP: {value: "sm9", label: "Team Up"},
+  DETECTIVE_PIKACHU: {value: "det1", label: "Detective Pikachu"},
+  UNBROKEN_BONDS: {value: "sm10", label: "Unbroken Bonds"},
+  UNIFIED_MINDS: {value: "sm11", label: "Unified Minds"},
+  HIDDEN_FATES: {value: "sma", label: "Shiny Vault"},
+  HIDDEN_FATES_SHINY: {value: "sm115", label: "Hidden Fates"},
+  COSMIC_ECLIPSE: {value: "sm12", label: "Cosmic Eclipse"},
+
+  // Sword & Shield Collection
+  SWSH_PROMO: {value: "swshp", label: "SWSH Black Star Promos"},
+  SWORD_SHIELD: {value: "swsh1", label: "Sword & Shield"},
+  REBEL_CLASH: {value: "swsh2", label: "Rebel Clash"},
+  DARKNESS_ABLAZE: {value: "swsh3", label: "Darkness Ablaze"},
+  CHAMPIONS_PATH: {value: "swsh35", label: "Champion's Path"},
+  VIVID_VOLTAGE: {value: "swsh4", label: "Vivid Voltage"},
+  SHINING_FATES: {value: "swsh45sv", label: "Shiny Vault"},
+  SHINING_FATES_SHINY: {value: "swsh45", label: "Shining Fates"},
+  BATTLE_STYLES: {value: "swsh5", label: "Battle Styles"}
+
+} as const;
