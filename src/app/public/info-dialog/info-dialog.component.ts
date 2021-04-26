7c177d37acee7768a652f529567f3b3037da9f2d
Stephanie Fay <stephaniemfay@gmail.com>
2021-04-26 11:25:35 -0300

    correcting some problems of responsiveness on the layouts. Overall status view finished.

---
+++ src/app/public/info-dialog/info-dialog.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+import {DynamicDialogConfig, DynamicDialogRef} from "primeng/dynamicdialog";
+import {CardAPI} from "../../models/cardAPI";
+
+export interface CardContent {
+  number?: string;
+  name?: string;
+  collectionName?: string;
+  collectionId?: string;
+  type?: string;
+  rarity?: string;
+  price?: string;
+  image?: string;
+  quantity?: string;
+}
+
+@Component({
+  selector: 'app-info-dialog',
+  templateUrl: './info-dialog.component.html',
+  styleUrls: ['./info-dialog.component.scss']
+})
+export class InfoDialogComponent implements OnInit {
+
+  constructor(public ref: DynamicDialogRef, public config: DynamicDialogConfig) { }
+
+  headers = [
+    {label: 'Card #', field: 'number'},
+    {label: 'Card Name', field: 'name'},
+    {label: 'Type', field: 'type'},
+    {label: 'Rarity', field: 'rarity'},
+    {label: 'Price (Un)', field: 'price'},
+    {label: 'Quantity', field: 'quantity'},
+    {label:'Image', field: 'image'}
+  ];
+
+  content: CardContent[];
+  loading = true;
+
+  ngOnInit(): void {
+    const cards: CardAPI[] = this.config.data;
+    this.content = [];
+    cards.forEach( card => {
+      let content: CardContent = {};
+      let types: string = '';
+      if (card.subtypes) {
+        card.subtypes.forEach(value => {
+          types += value + ', ';
+        });
+        types = types.slice(0, -2);
+      }
+
+      content.number = card.number;
+      content.name = card.name;
+      content.type = types;
+      content.rarity = card.rarity;
+      content.collectionName = card.set.name;
+      content.collectionId = card.set.id;
+      content.quantity = card.cardCSV.quantity.toString();
+      content.price = this.getPrice(card);
+      content.image = card.images.large;
+
+      this.content.push(content);
+    });
+    this.loading = false;
+  }
+
+  getPrice (card: CardAPI) {
+    if (card.cardCSV.extras && card.cardCSV.extras.length > 0 && card.cardCSV.extras.includes("Foil")) {
+      return Number(card.tcgplayer.prices['holofoil'].market).toFixed(2);
+    } else if (card.cardCSV.extras && card.cardCSV.extras.length > 0 && card.cardCSV.extras.includes("Reverse Foil")) {
+      if (card.tcgplayer.prices['reverseHolofoil']) {
+        return Number(card.tcgplayer.prices['reverseHolofoil'].market).toFixed(2);
+      } else {
+        for (let pricesKey in card.tcgplayer.prices) {
+          return Number(card.tcgplayer.prices[pricesKey].market).toFixed(2);
+        }
+      }
+    } else if (card.tcgplayer.prices['normal']) {
+      return Number(card.tcgplayer.prices['normal'].market).toFixed(2);
+    } else {
+      return '0.00';
+    }
+  }
+
+  goToLink(url: string){
+    window.open(url, "_blank");
+  }
+
+  getBadge (rarity:string) {
+    rarity = rarity.replace(/ /g, '').replace(/\./g, '').toLocaleLowerCase();
+    return rarity;
+  }
+
+}
---
+++ src/app/public/overall-status/overall-status.component.ts
@@ -0,0 +1,307 @@
+import { Component, OnInit } from '@angular/core';
+import {ApiCardService} from "../../services/api-card.service";
+import {CardAPI} from "../../models/cardAPI";
+import {DialogService} from "primeng/dynamicdialog";
+import {InfoDialogComponent} from "../info-dialog/info-dialog.component";
+import {CollectionsFunctions} from "../../models/collections";
+
+@Component({
+  selector: 'app-overall-status',
+  templateUrl: './overall-status.component.html',
+  styleUrls: ['./overall-status.component.scss']
+})
+export class OverallStatusComponent implements OnInit {
+
+  constructor(private service: ApiCardService,
+              public dialogService: DialogService) { }
+
+  loading = true;
+
+  // quantity of cards (general)
+  qtdTotalCards = 0;
+  qtdTotalUniqueCards = 0;
+  qtdTotalTrainerCards = 0;
+  qtdTotalPokemonCards = 0;
+  qtdTotalEnergyCards = 0;
+
+  energyMap = new Map();
+  trainerMap = new Map();
+  pokemonMap = new Map();
+  stagesMap = new Map();
+  collectionMap = new Map();
+  qualityMap = new Map();
+  rarityMap = new Map();
+
+  energyArray = [];
+  energyIndexArray = [];
+  trainerArray = [];
+  trainerIndexArray = [];
+  pokemonArray = [];
+  pokemonIndexArray = [];
+  stagesArray = [];
+  stagesIndexArray = [];
+  collectionArray = [];
+  collectionIndexArray = [];
+  qualityArray = [];
+  qualityIndexArray = [];
+  rarityArray = [];
+  rarityIndexArray = [];
+
+  // prices + fake
+  fakeCards = [];
+  totalPriceLow = 0;
+  totalPriceMedium = 0;
+  totalPriceHigh = 0;
+  totalPriceMarket = 0;
+
+  ngOnInit(): void {
+    this.service.getAll().subscribe(content => {
+      content.forEach(card => {
+        this.qtdTotalUniqueCards += 1;
+        this.qtdTotalCards += card.cardApi.cardCSV.quantity;
+        if (card.cardApi.supertype == 'Energy') {
+          this.qtdTotalEnergyCards += card.cardApi.cardCSV.quantity;
+          this.setTypeEnergy(card.cardApi);
+        } else if (card.cardApi.supertype == 'Trainer') {
+          this.qtdTotalTrainerCards += card.cardApi.cardCSV.quantity;
+          this.setTypeTrainer(card.cardApi);
+        } else if (card.cardApi.supertype == 'Pokémon') {
+          this.qtdTotalPokemonCards += card.cardApi.cardCSV.quantity;
+          this.setPokemonStages(card.cardApi);
+          this.setPokemonTypes(card.cardApi);
+        }
+        this.setPrices(card.cardApi);
+        this.setCollection(card.cardApi);
+        this.setQuality(card.cardApi);
+        this.setRarity(card.cardApi);
+      });
+      this.configureArrays(this.pokemonMap, this.pokemonArray, this.pokemonIndexArray);
+      this.configureArrays(this.stagesMap, this.stagesArray, this.stagesIndexArray);
+      this.configureArrays(this.energyMap, this.energyArray, this.energyIndexArray);
+      this.configureArrays(this.trainerMap, this.trainerArray, this.trainerIndexArray);
+      this.configureArrays(this.qualityMap, this.qualityArray, this.qualityIndexArray);
+      this.configureArrays(this.rarityMap, this.rarityArray, this.rarityIndexArray);
+      this.configureArrays(this.collectionMap, this.collectionArray, this.collectionIndexArray);
+
+      this.loading = false;
+    });
+  }
+
+  setTypeEnergy (card: CardAPI) {
+    card.subtypes.forEach(type => {
+      const energyTypeArray: CardAPI[] = this.energyMap.get(type);
+      if (energyTypeArray) {
+        energyTypeArray.push(card);
+        this.energyMap.set(type, energyTypeArray);
+      } else {
+        const newEnergyTypeArray = [];
+        newEnergyTypeArray.push(card);
+        this.energyMap.set(type, newEnergyTypeArray);
+      }
+    });
+  }
+
+  setTypeTrainer (card: CardAPI) {
+    card.subtypes.forEach(type => {
+      const trainerTypeArray: CardAPI[] = this.trainerMap.get(type);
+      if (trainerTypeArray) {
+        trainerTypeArray.push(card);
+        this.trainerMap.set(type, trainerTypeArray);
+      } else {
+        const newTrainerTypeArray = [];
+        newTrainerTypeArray.push(card);
+        this.trainerMap.set(type, newTrainerTypeArray);
+      }
+    });
+  }
+
+  setPokemonTypes (card: CardAPI) {
+    card.types.forEach(type => {
+      const pokemonTypeArray: CardAPI[] = this.pokemonMap.get(type);
+      if (pokemonTypeArray) {
+        pokemonTypeArray.push(card);
+        this.pokemonMap.set(type, pokemonTypeArray);
+      } else {
+        const newPokemonTypeArray = [];
+        newPokemonTypeArray.push(card);
+        this.pokemonMap.set(type, newPokemonTypeArray);
+      }
+    });
+  }
+
+  setPrices (card: CardAPI) {
+    let high: number;
+    let medium: number;
+    let low: number;
+    let market: number;
+    if (card.cardCSV.extras && card.cardCSV.extras.length > 0 && card.cardCSV.extras.includes("Foil")) {
+      high = Number(card.tcgplayer.prices['holofoil'].high);
+      medium = Number(card.tcgplayer.prices['holofoil'].mid);
+      low = Number(card.tcgplayer.prices['holofoil'].low);
+      market = Number(card.tcgplayer.prices['holofoil'].market);
+    } else if (card.cardCSV.extras && card.cardCSV.extras.length > 0 && card.cardCSV.extras.includes("Reverse Foil")) {
+      if (card.tcgplayer.prices['reverseHolofoil']) {
+        high = Number(card.tcgplayer.prices['reverseHolofoil'].high);
+        medium = Number(card.tcgplayer.prices['reverseHolofoil'].mid);
+        low = Number(card.tcgplayer.prices['reverseHolofoil'].low);
+        market = Number(card.tcgplayer.prices['reverseHolofoil'].market);
+      } else {
+        for (let pricesKey in card.tcgplayer.prices) {
+          high = Number(card.tcgplayer.prices[pricesKey].high);
+          medium = Number(card.tcgplayer.prices[pricesKey].mid);
+          low = Number(card.tcgplayer.prices[pricesKey].low);
+          market = Number(card.tcgplayer.prices[pricesKey].market);
+          break;
+        }
+      }
+    } else if (card.tcgplayer.prices['normal']) {
+      high = Number(card.tcgplayer.prices['normal'].high);
+      medium = Number(card.tcgplayer.prices['normal'].mid);
+      low = Number(card.tcgplayer.prices['normal'].low);
+      market = Number(card.tcgplayer.prices['normal'].market);
+    } else {
+      high = 0;
+      medium = 0;
+      low = 0;
+      market = 0;
+      this.fakeCards.push(card);
+    }
+
+    high = high * card.cardCSV.quantity;
+    medium = medium * card.cardCSV.quantity;
+    low = low * card.cardCSV.quantity;
+    market = market * card.cardCSV.quantity;
+
+    this.totalPriceHigh += high;
+    this.totalPriceMedium += medium;
+    this.totalPriceLow += low;
+    this.totalPriceMarket += market;
+  }
+
+  setPokemonStages (card: CardAPI) {
+    card.subtypes.forEach(type => {
+      const pokemonStageArray: CardAPI[] = this.stagesMap.get(type);
+      if (pokemonStageArray) {
+        pokemonStageArray.push(card);
+        this.stagesMap.set(type, pokemonStageArray);
+      } else {
+        const newPokemonStageArray = [];
+        newPokemonStageArray.push(card);
+        this.stagesMap.set(type, newPokemonStageArray);
+      }
+    });
+  }
+
+  setCollection (card: CardAPI) {
+    const collectionArray: CardAPI[] = this.collectionMap.get(card.set.id);
+    if (collectionArray) {
+      collectionArray.push(card);
+      this.collectionMap.set(card.set.id, collectionArray);
+    } else {
+      const newCollectionArray: CardAPI[] = [];
+      newCollectionArray.push(card);
+      this.collectionMap.set(card.set.id, newCollectionArray);
+    }
+  }
+
+  setQuality (card: CardAPI) {
+    const qualityArray: CardAPI[] = this.qualityMap.get(card.cardCSV.quality);
+    if (qualityArray) {
+      qualityArray.push(card);
+      this.qualityMap.set(card.cardCSV.quality, qualityArray);
+    } else {
+      const newQualityArray: CardAPI[] = [];
+      newQualityArray.push(card);
+      this.qualityMap.set(card.cardCSV.quality, newQualityArray);
+    }
+  }
+
+  setRarity (card: CardAPI) {
+    const rarityArray: CardAPI[] = this.rarityMap.get(card.rarity);
+    if (rarityArray) {
+      rarityArray.push(card);
+      this.rarityMap.set(card.rarity, rarityArray);
+    } else {
+      const newRarityArray: CardAPI[] = [];
+      newRarityArray.push(card);
+      this.rarityMap.set(card.rarity, newRarityArray);
+    }
+  }
+
+  configureArrays (map: Map<string, any[]>, array: any[], indexArray: any[]) {
+    let auxiliar = {};
+    map.forEach((value: any[], key: string) => {
+      auxiliar[key] = value;
+      indexArray.push(key);
+    });
+    indexArray.sort();
+    array.push(auxiliar);
+  }
+
+  openModal(key: string, type: string) {
+    switch (type) {
+      case 'pokemon':
+        this.dialogService.open(InfoDialogComponent, {
+          header: 'Cards in this section',
+          width: '70%',
+          data: this.pokemonMap.get(key)
+        });
+        break;
+      case 'trainer':
+        this.dialogService.open(InfoDialogComponent, {
+          header: 'Cards in this section',
+          width: '70%',
+          data: this.trainerMap.get(key)
+        });
+        break;
+      case 'energy':
+        this.dialogService.open(InfoDialogComponent, {
+          header: 'Cards in this section',
+          width: '70%',
+          data: this.energyMap.get(key)
+        });
+        break;
+      case 'stage':
+        this.dialogService.open(InfoDialogComponent, {
+          header: 'Cards in this section',
+          width: '70%',
+          data: this.stagesMap.get(key)
+        });
+        break;
+      case 'collection':
+        this.dialogService.open(InfoDialogComponent, {
+          header: 'Cards in this section',
+          width: '70%',
+          data: this.collectionMap.get(key)
+        });
+        break;
+      case 'rarity':
+        this.dialogService.open(InfoDialogComponent, {
+          header: 'Cards in this section',
+          width: '70%',
+          data: this.rarityMap.get(key)
+        });
+        break;
+      case 'quality':
+        this.dialogService.open(InfoDialogComponent, {
+          header: 'Cards in this section',
+          width: '70%',
+          data: this.qualityMap.get(key)
+        });
+        break;
+      case 'fake':
+        this.dialogService.open(InfoDialogComponent, {
+          header: 'Cards in this section',
+          width: '70%',
+          data: this.fakeCards
+        });
+        break;
+    }
+  }
+
+  getCollectionName (key: string) {
+    const func = new CollectionsFunctions();
+    return func.getCollectionByIdentifier(key);
+  }
+
+}
